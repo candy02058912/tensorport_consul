e6afccda564038881d7fd383f606b61d0fd78346
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2017-12-01 05:27:53 +0100

    Initial Deployment

---
+++ __init__.py
@@ -0,0 +1,151 @@
+from os.path import join
+from tensorport_consul.api import put_kv, get_kv, get_kv_cas, get_kv_meta, delete_kv, put_kv_txn
+from tensorport_consul.serializer import map_dictionary, dictionary_map
+from tensorport_consul.settings import DEFAULT_ENDPOINT, DEFAULT_REQUEST_TIMEOUT
+
+
+class Connection(object):
+    """
+    Client for the consul key value store API
+    """
+    endpoint = DEFAULT_ENDPOINT
+    timeout = DEFAULT_REQUEST_TIMEOUT
+
+    def __init__(self, endpoint=None, timeout=None, auth=None):
+        self.endpoint = endpoint or self.endpoint
+        self.timeout = timeout or DEFAULT_REQUEST_TIMEOUT
+        self.auth = auth
+
+    def put(self, k, v, cas=None):
+        """
+        Put a key value pair at the configured endpoint
+        :param str k: key to put
+        :param int cas: the cas version number. If None, no cas is used.
+        :param str v: value to put
+        :return None:
+        """
+        return put_kv(
+            k, v, cas,
+            endpoint=join(self.endpoint, 'kv/'),
+            timeout=self.timeout
+        )
+
+    def put_mapping(self, mapping, verb='set'):
+        """
+        Atomically (Txn) put a key/value mapping at the configured endpoint
+        :param dict mapping: dict of key/values put
+        :param str verb: The type of operation to perform. See the list of possibilities
+        here https://www.consul.io/docs/agent/http/kv.html#txn
+        :return None:
+        """
+        return put_kv_txn(
+            mapping,
+            endpoint=join(self.endpoint, 'txn'),
+            verb=verb,
+            timeout=self.timeout
+        )
+
+    def put_dict(self, dictionary, verb='set'):
+        """
+        Atomically (Txn) put a dict at the configured endpoint
+        :param dict dictionary: dict of nested keys and values
+        {'some': {'key1': 'value1', 'key2': 'value2'} ->
+        [{'some/key1': 'value1, 'some/key2': 'value2}]
+        :param str verb: The type of operation to perform. See the list of possibilities
+        here https://www.consul.io/docs/agent/http/kv.html#txn
+        :return None:
+        """
+        mapping = map_dictionary(dictionary)
+        return self.put_mapping(mapping, verb=verb)
+
+    def get_cas(self, k=None, recurse=False):
+        """
+        Get a value for a key and use CAS to guard updates against updates
+        from multiple clients.
+        :param str k: key to get
+        :param bool recurse: return nested entries
+        :return dict mapping: retrieved key/value mapping
+        """
+        return get_kv_cas(
+            k=k, recurse=recurse,
+            endpoint=join(self.endpoint, 'kv/'),
+            timeout=self.timeout,
+            auth=self.auth
+        )
+
+    def get_meta(self, k=None, recurse=False):
+        """
+        Get the raw un-decoded key value data for a key
+        :param str k: key to get
+        :param bool recurse: return nested entries
+        :return dict: raw API response
+        """
+        return get_kv_meta(
+            k=k, recurse=recurse,
+            endpoint=self.endpoint,
+            timeout=self.timeout,
+            auth=self.auth
+        )
+
+    def get(self, k=None, recurse=False):
+        """
+        Get a key or all keys if recursive is specified
+        :param str k: key to get
+        :param bool recurse: return nested entries
+        :return dict mapping: retrieved key/value mapping
+        """
+        return get_kv(
+            k=k, recurse=recurse,
+            endpoint=join(self.endpoint, 'kv/'),
+            timeout=self.timeout,
+            auth=self.auth
+        )
+
+    def get_value(self, k=None, recurse=False):
+        """
+        Get a value for a key or all values under that key if recursive is specified
+        :param str k: key to get
+        :param bool recurse: return nested entries
+        :return dict mapping: retrieved key/value mapping
+        """
+        return list(get_kv(
+            k=k, recurse=recurse,
+            endpoint=join(self.endpoint, 'kv/'),
+            timeout=self.timeout,
+            auth=self.auth
+        ).values())[0]
+
+    def get_mapping(self, k=None):
+        """
+        Retrieve a key value mapping from a specified key
+        Note: contrary to put_mapping, this method is not atomic
+        :param k: The key to get recursively and return as a key value mapping
+        :return dict mapping: The retrieved key value mapping
+        """
+        return self.get(k=k, recurse=True)
+
+    def get_dict(self, k=None):
+        """
+        Retrieve a dict that represents nested key value entries from the specified
+        endpoint. [{'some/key1': 'value1, 'some/key2': 'value2}] ->
+        {'some': {'key1': 'value1', 'key2': 'value2'}
+        Note: contrary to put_dict, this method is not atomic
+        :param k: The key to get recursively and return as a dict
+        :return dict dictionary: The retrieved dict of nested keys and values
+        """
+        mapping = self.get_mapping(k=k)
+        return dictionary_map(mapping)
+
+    def delete(self, k=None, recurse=False):
+        """
+        Delete the specified key or all values under that key if recursive is specified
+        :param str k: key to delete
+        :param bool recurse: delete nested entries
+        :return None:
+        """
+        return delete_kv(
+            k=k, recurse=recurse,
+            endpoint=join(self.endpoint, 'kv/'),
+            timeout=self.timeout,
+            auth=self.auth
+        )
---
+++ api.py
@@ -0,0 +1,197 @@
+import base64
+import socket
+import ssl
+from base64 import b64decode, b64encode
+from json import loads, dumps
+from logging import getLogger
+from os.path import join
+from urllib import request
+from urllib.parse import urlencode
+
+import requests
+
+from tensorport_consul.settings import DEFAULT_KV_ENDPOINT, DEFAULT_TXN_ENDPOINT
+
+log = getLogger(__name__)
+
+
+def put_kv(
+    k, v, cas=None, endpoint=DEFAULT_KV_ENDPOINT,
+    timeout=socket._GLOBAL_DEFAULT_TIMEOUT
+):
+    """
+    Put a key and value to the distributed key value store at the location path
+    :param str k: the key to put
+    :param int cas: the cas version number. If None, no cas is used.
+    :param str v: the value to put
+    :param str endpoint: API url to PUT to
+    :param int timeout: Seconds before timing out
+    :return None:
+    """
+    encoded = str.encode(str(v))
+    params = dict()
+    if cas:
+        params['cas'] = cas
+
+    url = join(endpoint, k) if k else endpoint
+
+    if params:
+        url = "{}/?{}".format(url, urlencode(params))
+
+    req = request.Request(
+        url=url, data=encoded, method='PUT'
+    )
+    with request.urlopen(req, timeout=timeout) as f:
+        log.debug("PUT k v pair ({}, {}) to {}: {}, {}".format(
+            k, v, url, f.status, f.reason
+        ))
+
+
+def _mapping_to_txn_data(mapping, verb='set'):
+    """
+    Transform a key value mapping to a list of operations to perform
+    inside the atomic transaction.
+    :param dict mapping: flat dict of key/values, or key/(value, modification-index, verb)
+    :param str verb: The type of operation to perform. See the list of possibilities
+    here https://www.consul.io/docs/agent/http/kv.html#txn
+    :return list[dict, ..] txn_data: List of dicts describing the operations to perform
+    """
+    txn_data = [
+        {
+            'KV': {
+                'Verb': verb,
+                'Key': k,
+                'Value': b64encode(str(v).encode('utf-8')).decode('utf-8'),
+            }
+        } for k, v in mapping.items()
+    ]
+    if verb == 'cas':
+        # If the index is 0, Consul will only put the key if it does not already exist.
+        # See https://www.consul.io/api/kv.html#create-update-key
+        for item in txn_data:
+            item['Index'] = 0
+    return txn_data
+
+
+def put_kv_txn(mapping, endpoint=DEFAULT_TXN_ENDPOINT, verb='set', timeout=socket._GLOBAL_DEFAULT_TIMEOUT):
+    """
+    Update multiple keys inside a single, atomic transaction.
+    The body of the request should be a list of operations to
+    perform inside the atomic transaction. Up to 64 operations
+    may be present in a single transaction. Each Base64-encoded
+    blob of data can not be larger than 512kB.
+    https://www.consul.io/docs/agent/http/kv.html
+    :param dict mapping: flat dict of key/values put
+    :param str endpoint: API url to PUT to. Should be a txn endpoint.
+    :param str verb: The type of operation to perform. See the list of possibilities
+    here https://www.consul.io/docs/agent/http/kv.html#txn
+    :param int timeout: Seconds before timing out
+    :return None:
+    """
+    txn_data = _mapping_to_txn_data(mapping, verb=verb)
+    data = dumps(txn_data).encode('utf-8')
+    req = request.Request(
+        url=endpoint, data=data, method='PUT',
+        headers={'Content-Type': 'application/json'}
+    )
+    with request.urlopen(req, timeout=timeout) as f:
+        log.debug("PUT k v mapping {} to {}: {}, {}".format(
+            mapping, endpoint, f.status, f.reason
+        ))
+
+
+def get_kv_builder(postprocessor=lambda x: x):
+    """
+    Function that returns a function that gets the key value mapping
+    from the distributed key value store and postprocesses the result
+    before returning the value.
+    :param func postprocessor: The function to apply to the result
+    before returning it as the result of the returned function
+    The default postprocessor simply returns the value as is.
+    :return func get_kv_raw: The function that gets the key value
+    and applies the passed postprocessor before returning the retrieved
+    value
+    """
+    def get_kv_raw(
+        k=None, recurse=False, endpoint=DEFAULT_KV_ENDPOINT,
+        timeout=socket._GLOBAL_DEFAULT_TIMEOUT, auth=None
+    ):
+        """
+        Get the key value mapping from the distributed key value store and
+        apply the defined postprocessor from the outer context before
+        returning the retrieved value.
+        :param str k: key to get
+        :param bool recurse: whether or not to recurse over the path and
+        retrieve all nested values
+        :param str endpoint: API url to get the value from
+        :param int timeout: Seconds before timing out
+        :return dict|mul mapping: key value mapping, or the return value
+        as defined in the specified postprocessor of the outer function.
+        """
+        url = join(endpoint, k) if k else endpoint
+        url = "{}/?recurse".format(url) if recurse else url
+
+        session = requests.Session()
+        if auth:
+            session.auth = auth
+
+        r = session.get(url)
+
+        result = r.json()
+        return postprocessor(result)
+    return get_kv_raw
+
+get_kv_meta = get_kv_builder()
+
+get_kv_cas = get_kv_builder(
+    lambda x: {
+        r['Key']: (b64decode(r['Value']).decode('utf-8'), r['ModifyIndex'])
+        if r['Value'] else None for r in x if r['Key']
+    }
+)
+
+get_kv = get_kv_builder(
+    lambda x: {
+        # values are stored base64 encoded in consul, they
+        # are decoded before returned by this function.
+        r['Key']: b64decode(r['Value']).decode('utf-8')
+        if r['Value'] else None for r in x if r['Key']
+    }
+)
+
+
+def delete_kv(
+    k=None, cas=None, recurse=False, endpoint=DEFAULT_KV_ENDPOINT,
+    timeout=socket._GLOBAL_DEFAULT_TIMEOUT, auth=None
+):
+    """
+    Delete a key from the distributed key value store
+    :param str k: the key to delete
+    :param int cas: the cas version number. If None, no cas is used.
+    :param bool recurse: recurse the path and delete all entries
+    :param str endpoint: API url to DELETE
+    :param int timeout: Seconds before timing out
+    :return:
+    """
+    params = dict()
+    if cas:
+        params['cas'] = cas
+    if recurse:
+        params['recurse'] = recurse
+
+    url = join(endpoint, k) if k else endpoint
+
+    if params:
+        url = "{}/?{}".format(url, urlencode(params))
+
+    req = request.Request(
+        url=url,
+        method='DELETE'
+    )
+    with request.urlopen(req, timeout=timeout) as f:
+        log.debug("DELETEd key {}{}: {} {}".format(
+            url,
+            ' recursively' if recurse else '',
+            f.status,
+            f.reason
+        ))
